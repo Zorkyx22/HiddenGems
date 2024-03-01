8e5703c9668cf273fc91ce5b3a31a3aaec246264
Zorkyx22 <icu.zorkyx@gmail.com>
2024-03-01 17:12:27 -0500

    IT WORKS

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use clap::{Parser, Subcommand};
 use image::{DynamicImage, GenericImage, GenericImageView, ImageError, Rgba};
 
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[clap(version)]
 struct Arguments {
     #[command(subcommand)]
     cmd: Commands,
 }    
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
     Decode {
         #[clap(short='p', long)]
@@ -36,24 +36,26 @@ fn modify_pixels(img: &DynamicImage, content: Vec<u8>) -> Result<DynamicImage, B
     let total_bits: u32 = img_dimensions.0*img_dimensions.1;
     assert!(content.len() as u32 * 8 <= total_bits);
     
+    
     let bit_buff: BitVec = content.into_iter()
         .flat_map(|byte| 
             (0..8).rev()
                 .map(move |i| 
                     (byte >> i) &  1 !=  0)).collect();
-
+    
+    
     for (i, bits) in bit_buff.chunks(3).enumerate() {
         let x = i as u32 % img_dimensions.0;
         let y = i as u32 / img_dimensions.0;
         let mut pixel = img.get_pixel(x, y).0;
-        for (pos, bit) in bits.iter().by_vals().enumerate() {
+                for (pos, bit) in bits.iter().by_vals().enumerate() {
             if bit {
                 pixel[pos] |= 1;
             } else {
                 pixel[pos] &= 254;
             }
         }
-        new_img.put_pixel(x, y, Rgba::<u8>(pixel))
+                new_img.put_pixel(x, y, Rgba::<u8>(pixel))
     }
     Ok(new_img)
 }
@@ -83,9 +85,9 @@ fn read_hidden_content(file: PathBuf) -> Result<Vec<u8>, ImageError> {
     for (_, _, pixel) in img.pixels() {
         bitstream.extend([pixel.0[0]&1!=0, pixel.0[1]&1!=0, pixel.0[2]&1!=0].iter());
     }
-    let bytes: Vec<u8> = bitstream.chunks_exact(8).map(|chunk| {
+        let bytes: Vec<u8> = bitstream.chunks_exact(8).map(|chunk| {
         let mut byte =  0u8;
-        for (i, &bit) in chunk.iter().enumerate() {
+        for (i, &bit) in chunk.iter().rev().enumerate() {
             if bit {
                 byte |=  1 << i;
             }
@@ -100,11 +102,13 @@ fn read_to_file(file: PathBuf, output_path: Option<PathBuf>) -> Result<bool, Box
     let success: bool;
     match String::from_utf8(extraction.clone()) {
         Ok(s) => {
+            println!("Success");
             success = redirect_output(s, output_path)?;
         },
         Err(e) => {
             let valid_up_to = e.utf8_error().valid_up_to();
             let s = String::from_utf8_lossy(&extraction[..valid_up_to]).into_owned();
+            println!("Failed until {} and s ={}", valid_up_to, s);
             success = redirect_output(s, output_path)?;
         }
     }
@@ -113,7 +117,7 @@ fn read_to_file(file: PathBuf, output_path: Option<PathBuf>) -> Result<bool, Box
 
 fn main() -> Result<(), Box<dyn Error>>{
     let args = Arguments::parse();
-    match args.cmd {
+        match args.cmd {
         Commands::Decode {path, output_path} => {
             read_to_file(path, output_path)?;
         }
